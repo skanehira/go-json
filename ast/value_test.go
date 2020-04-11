6c86754271d607fc4c68d99e8509140609f1428d
skanehira <sho19921005@gmail.com>
2020-04-12 02:54:06 +0900

    add array test

--- ast/value_test.go
+++ ast/value_test.go
@@ -28,6 +28,7 @@ func TestObject(t *testing.T) {
 	}
 
 	o = Object{
+		Type: token.Object,
 		Elements: []Element{
 			{
 				Name: "name",
@@ -80,6 +81,54 @@ func TestArray(t *testing.T) {
 	}
 
 	want := "[]"
+	if a.String() != want {
+		t.Fatalf("want=%s, got=%s", want, a.String())
+	}
+
+	a = Array{
+		Type: token.Array,
+		Values: []Value{
+			Object{
+				Type: token.Object,
+				Elements: []Element{
+					{
+						Name: "name",
+						Value: String{
+							Type:  token.String,
+							Value: "gorilla",
+						},
+					},
+				},
+			},
+			Array{
+				Type: token.Array,
+				Values: []Value{
+					Int{
+						Type:  token.Int,
+						Value: 1,
+					},
+				},
+			},
+			Int{
+				Type:  token.Int,
+				Value: 10,
+			},
+			String{
+				Type:  token.String,
+				Value: "gorilla",
+			},
+			Bool{
+				Type:  token.Bool,
+				Value: false,
+			},
+			Null{
+				Type: token.Null,
+			},
+		},
+	}
+
+	want = `[{"name":"gorilla"},[1],10,"gorilla",false,null]`
+
 	if a.String() != want {
 		t.Fatalf("want=%s, got=%s", want, a.String())
 	}
