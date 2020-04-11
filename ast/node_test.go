a84552ee33aaa83f6a2b3e9719548a32a2d97840
skanehira <sho19921005@gmail.com>
2020-04-12 02:02:33 +0900

    add node test

---
+++ ast/node_test.go
@@ -0,0 +1,39 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/skanehira/go-json/token"
+)
+
+func TestString(t *testing.T) {
+	want := `"a"`
+	str := String{
+		Value: "a",
+	}
+
+	if str.String() != want {
+		t.Fatalf("want=%s, got=%s", want, str.String())
+	}
+}
+
+func TestObject(t *testing.T) {
+	o := Object{
+		Elements: map[string]Node{
+			"name": String{
+				Type:  token.String,
+				Value: "gorilla",
+			},
+			"age": Int{
+				Type:  token.Int,
+				Value: 1,
+			},
+		},
+	}
+
+	want := `{"name":"gorilla","age":1}`
+
+	if o.String() != want {
+		t.Fatalf("want=%s, got=%s", want, o.String())
+	}
+}
